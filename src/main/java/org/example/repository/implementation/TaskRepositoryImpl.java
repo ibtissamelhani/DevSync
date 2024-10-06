011d5b92ae63494a9f223d17ed395ae9462a00d3
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-10-06 15:41:10 +0200

    Task repository implementations added

--- src/main/java/org/example/repository/implementation/TaskRepositoryImpl.java
+++ src/main/java/org/example/repository/implementation/TaskRepositoryImpl.java
@@ -2,11 +2,11 @@ package org.example.repository.implementation;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.TypedQuery;
 import org.example.model.entities.Task;
 import org.example.repository.interfaces.TaskRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class TaskRepositoryImpl implements TaskRepository {
 
@@ -33,19 +33,46 @@ public class TaskRepositoryImpl implements TaskRepository {
     }
 
     @Override
-    public Task findById(long id) {
+    public Optional<Task> findById(long id) {
         try(EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            return entityManager.find(Task.class, id) ;
+            Task task = entityManager.find(Task.class, id);
+            return Optional.ofNullable(task);
         }
     }
 
-//    @Override
-//    public List<Task> findAll() {
-//        try(EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-//            TypedQuery<Task> query = entityManager.createQuery("SELECT t FROM Task t", Task.class);
-//
-//        }
-//    }
+    @Override
+    public List<Task> findAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("SELECT t FROM Task t", Task.class)
+                    .getResultList();
+        }
+    }
 
+    @Override
+    public void delete(Task task) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityManager.getTransaction().begin();
+            Task managedTask = entityManager.merge(task);
+            entityManager.remove(managedTask);
+            entityManager.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void update(Task task) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try  {
+            entityManager.getTransaction().begin();
+            entityManager.merge(task);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+        }
+        throw e;
+        } finally {
+            entityManager.close();
+        }
+    }
 
 }
--- src/main/java/org/example/repository/interfaces/TaskRepository.java
+++ src/main/java/org/example/repository/interfaces/TaskRepository.java
@@ -2,11 +2,14 @@ package org.example.repository.interfaces;
 
 import org.example.model.entities.Task;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository {
 
     void save(Task task);
-    Task findById(long id);
-//    List<Task> findAll();
-//    void delete(Task task);
-//    void update(Task task);
+    Optional<Task> findById(long id);
+    List<Task> findAll();
+    void delete(Task task);
+    void update(Task task);
 }
