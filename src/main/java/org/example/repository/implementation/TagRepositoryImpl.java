8bf111479a2b0f3660779ab2d8b52fe42c04d7ca
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-10-06 18:02:34 +0200

    Tag repository implementations added

--- src/main/java/org/example/repository/implementation/TagRepositoryImpl.java
+++ src/main/java/org/example/repository/implementation/TagRepositoryImpl.java
@@ -1,4 +1,78 @@
 package org.example.repository.implementation;
 
-public class TagRepositoryImpl {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.example.model.entities.Tag;
+import org.example.repository.interfaces.TagRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TagRepositoryImpl implements TagRepository {
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    public TagRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Override
+    public void save(Tag tag) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try  {
+            entityManager.getTransaction().begin();
+            entityManager.persist(tag);
+            entityManager.getTransaction().commit();
+        }catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            System.out.println(e.getMessage());
+        }finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public Optional<Tag> findById(Long id) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Tag tag = entityManager.find(Tag.class, id);
+            return Optional.ofNullable(tag);
+        }
+    }
+
+    @Override
+    public List<Tag> findAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("SELECT t FROM Tag t", Tag.class)
+                    .getResultList();
+        }
+    }
+
+    @Override
+    public void update(Tag tag) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try  {
+            entityManager.getTransaction().begin();
+            entityManager.merge(tag);
+            entityManager.getTransaction().commit();
+        }catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public void delete(Tag tag) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityManager.getTransaction().begin();
+            Tag managedTag = entityManager.merge(tag);
+            entityManager.remove(managedTag);
+            entityManager.getTransaction().commit();
+        }
+    }
 }
--- src/main/java/org/example/repository/interfaces/TagRepository.java
+++ src/main/java/org/example/repository/interfaces/TagRepository.java
@@ -1,4 +1,14 @@
 package org.example.repository.interfaces;
 
+import org.example.model.entities.Tag;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TagRepository {
+    void save(Tag tag);
+    Optional<Tag> findById(Long id);
+    List<Tag> findAll();
+    void update(Tag tag);
+    void delete(Tag tag);
 }
